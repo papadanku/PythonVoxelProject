f4bfa1d598e222d83e110e1cee0edbd6fe330d2e
papadanku <pauldang2@gmail.com>
2026-01-01 19:54:38 -0700

    Fix OpenGL context configuration by correcting the minor version attribute

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class VoxelEngine:
 
         # Configure OpenGL 3.3 Core context with 24-bit depth buffer
         pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
         pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
         pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE, 24)
 
